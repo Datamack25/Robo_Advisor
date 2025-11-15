733f0e45ac0bc7b25ef7fb5276b30d2bda3e6ca8
Datamack25 <mackensoncineus9@gmail.com>
2025-11-15 21:06:11 +0100

    Update risk_profiles.py

--- .streamlit/core/risk_profiles.py
+++ .streamlit/core/risk_profiles.py
@@ -1,1 +1,9 @@
+# core/risk_profiles.py
+def map_profile_to_risk(profile):
+    mapping = {
+        "Conservateur": 0.08,
+        "Modéré": 0.15,
+        "Agressif": 0.25
+    }
+    return mapping.get(profile, 0.15)
 
