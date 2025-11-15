6ba03fd5c35aa1ce89276857406ab8cb1f867470
Datamack25 <mackensoncineus9@gmail.com>
2025-11-15 20:18:08 +0100

    Create main.py

---
+++ .streamlit/app/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="RoboAdvisor IA", layout="wide")
+
+st.title("🤖 RoboAdvisor — Bienvenue")
+st.write("Ton application Streamlit est bien installée !")
